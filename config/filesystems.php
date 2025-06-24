bab044f92ad16b596e56baf802d60152013cb8c9
Joan Serna <13702219+JoanSernaLeiton@users.noreply.github.com>
2025-06-24 13:05:02 -0500

    fix filesystems

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DISK', 'caffelia-storage'),
+    'default' => env('FILESYSTEM_DISK', 'public'),
 
     /*
     |--------------------------------------------------------------------------
