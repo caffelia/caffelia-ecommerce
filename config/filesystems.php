73513a7f5928dbe7a448825f029ce5c64c05fb8f
Joan Serna <13702219+JoanSernaLeiton@users.noreply.github.com>
2025-06-24 12:29:44 -0500

    fix filesystems

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DISK', 'public'),
+    'default' => env('FILESYSTEM_DISK', 'caffelia-storage'),
 
     /*
     |--------------------------------------------------------------------------
@@ -47,7 +47,7 @@ return [
         'public' => [
             'driver'     => 'local',
             'root'       => storage_path('app/public'),
-            'url'        => env('APP_URL').'/storage',
+            'url'        => env('AWS_URL', env('APP_URL').'/storage'),
             'visibility' => 'public',
             'throw'      => false,
         ],
