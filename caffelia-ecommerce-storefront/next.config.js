2e58318d7178799bdda2f580ec845e35a48be096
Joan Serna <13702219+JoanSernaLeiton@users.noreply.github.com>
2025-07-31 23:25:46 -0500

    add s3 module

--- caffelia-ecommerce-storefront/next.config.js
+++ caffelia-ecommerce-storefront/next.config.js
@@ -36,6 +36,14 @@ const nextConfig = {
         protocol: "https",
         hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "caffelia-ecommerce-prod.s3.us-east-2.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "caffelia-ecommerce-dev.s3.us-east-2.amazonaws.com",
+      },
     ],
   },
 }
